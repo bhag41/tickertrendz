561ee0893a162d023505371c1180bc2ec23ea2cb
Bhagyashree Kulkarni <35288783+bhag41@users.noreply.github.com>
2025-03-28 14:30:50 -0700

    rename app.js to App.js and app.css to App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Stocks from './pages/Stocks';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-        Hello World
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<Home />} />
+      <Route path = '/stocks/:symbol' element = {<Stocks />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
